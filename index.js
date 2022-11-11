3c5e5473db283dd8b0c6e0345de3060136c89ab9
VUTIENTHANH-shyber <nguyenthuytrang01061996@gmail.com>
2022-11-11 21:47:33 +0700

    nop bai tap

---
+++ index.js
@@ -0,0 +1,6 @@
+let sotienvay = +prompt("Nhập số tiền vay");
+let laisuat = +prompt("Nhập lãi suất"); // phần trăm %
+let nam = +prompt("Nhập số năm");
+let tienlai = sotienvay * laisuat * nam;
+
+alert("Số tiền lãi là: " + tienlai) ;
